5229077be3c6668a1960b516bfc6d1b1dc22e6ec
Reyaansh Sinha <199857299+Reboy20000@users.noreply.github.com>
2026-09-07 13:21:21 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tangible Neuron
+Official Repo for the Tangible Neuron Project
+
+**NOTE**: This project is the Actual Verilog HDL based CPU, Not the Emulator. If you are willing to make code for Neuron, Use Nemu as this Repo is where the team will design the actual circut which then will be fabricated for production.
+
+Tangible Neuron is a Open source, ASIC based, AI based processor Designed to run Intense AI based workflows on **1** Chip
--- obj_dir/Vhello__ALL.d
+++
@@ -1,9 +0,0 @@
-Vhello__ALL.o: Vhello__ALL.cpp Vhello.cpp Vhello__pch.h \
-  /opt/homebrew/Cellar/verilator/5.052/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.052/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.052/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.052/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.052/share/verilator/include/verilated_funcs.h \
-  Vhello__Syms.h Vhello.h Vhello___024root.h Vhello___024root__0.cpp \
-  Vhello__main.cpp Vhello___024root__Slow.cpp \
-  Vhello___024root__0__Slow.cpp Vhello__Syms__Slow.cpp
